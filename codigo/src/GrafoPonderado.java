a630bdf4438d9d17a95eb0b01d21801d54a79c2b
JVTemponi <jvictortemponi@gmail.com>
2022-10-01 13:49:57 -0300

    adição do método add aresta ponderada

--- codigo/src/GrafoPonderado.java
+++ codigo/src/GrafoPonderado.java
@@ -2,7 +2,20 @@ public class GrafoPonderado extends GrafoMutavel{
 
     public GrafoPonderado(String nome) {
         super(nome);
-        //TODO Auto-generated constructor stub
+    }
+
+    public void addAresta(int origem, int destino, int peso){
+    
+        Vertice verticesArray[] = new Vertice[this.ordem()];
+        verticesArray = this.vertices.allElements(verticesArray);
+        for (Vertice orig : verticesArray) {
+
+            for (Vertice dest : verticesArray) {
+                if (orig.existeAresta(dest.getId()) != null) {
+               // Aresta ares = new Aresta(peso, dest.getId());
+                }
+            }
+        }
     }
     
 }
