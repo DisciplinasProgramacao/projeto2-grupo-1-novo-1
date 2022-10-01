07b11e3efeb6c9f7f3694a9efeac6c7cd41bddcb
JVTemponi <jvictortemponi@gmail.com>
2022-10-01 15:16:02 -0300

    Método adicionar aresta e subgrafo

--- codigo/src/GrafoPonderado.java
+++ codigo/src/GrafoPonderado.java
@@ -4,18 +4,32 @@ public class GrafoPonderado extends GrafoMutavel{
         super(nome);
     }
 
-    public void addAresta(int origem, int destino, int peso){
-    
-        Vertice verticesArray[] = new Vertice[this.ordem()];
-        verticesArray = this.vertices.allElements(verticesArray);
-        for (Vertice orig : verticesArray) {
+    public boolean addAresta(int origem, int destino, int peso){
 
-            for (Vertice dest : verticesArray) {
-                if (orig.existeAresta(dest.getId()) != null) {
-               // Aresta ares = new Aresta(peso, dest.getId());
-                }
+        boolean adicionou = false;
+        Vertice saida = this.existeVertice(origem);
+        Vertice chegada = this.existeVertice(destino);
+        if(saida!=null && chegada !=null){
+            saida.addAresta(destino,peso);
+            chegada.addAresta(origem,peso);
+            adicionou = true;
+        }
+        return adicionou;
+    }
+
+    @Override
+    public GrafoCompleto subGrafo(Lista<Vertice> vertices){
+        Vertice verticesArray[] = new Vertice[ordem()];
+        verticesArray = vertices.allElements(verticesArray);
+        int naoNull = 0;
+        for(int i = 0; i < this.ordem(); i++){
+            if(verticesArray[i] != null){
+                naoNull++;
             }
         }
+        if(naoNull <= ordem() && naoNull > 0){
+            return new GrafoCompleto("Subgrafo de " + this.nome, naoNull);
+        } else return null;
     }
-    
+
 }
