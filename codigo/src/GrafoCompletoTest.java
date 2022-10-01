792932a75ebf9a146aac86871cf4c875d8971503
JVTemponi <jvictortemponi@gmail.com>
2022-10-01 10:12:37 -0300

    Classe teste desenvolvida por Fabrício

---
+++ codigo/src/GrafoCompletoTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GrafoCompletoTest {
+
+
+    @Test
+    public void testarCriarGrafo(){
+        GrafoCompleto grafoTeste = new GrafoCompleto("Teste", 6);
+        Lista<Vertice> vertices = new Lista<>();
+        for(int i=0; i<4; i++){
+            vertices.add(grafoTeste.existeVertice(i));
+        }
+        GrafoCompleto subgrafoTeste = grafoTeste.subGrafo(vertices);
+        assertEquals(4, subgrafoTeste.ordem());
+    }
+}
