25fde2ebe279e27592829fbc3153c77de2358e63
JVTemponi <jvictortemponi@gmail.com>
2022-10-01 13:49:29 -0300

    remoção de imports não utilizados

--- codigo/src/GrafoNaoPonderadoTest.java
+++ codigo/src/GrafoNaoPonderadoTest.java
@@ -1,6 +1,4 @@
-import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
