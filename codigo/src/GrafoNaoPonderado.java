ff543c6e169f1444eadc30f1a4b44957260b4942
JVTemponi <jvictortemponi@gmail.com>
2022-10-01 15:15:06 -0300

    Override método subgrafo

--- codigo/src/GrafoNaoPonderado.java
+++ codigo/src/GrafoNaoPonderado.java
@@ -1,9 +1,21 @@
 public class GrafoNaoPonderado extends GrafoMutavel{
 
     public GrafoNaoPonderado(String nome) {
-        
         super(nome);
-        
     }
-    
+
+    @Override
+    public GrafoCompleto subGrafo(Lista<Vertice> vertices){
+        Vertice verticesArray[] = new Vertice[ordem()];
+        verticesArray = vertices.allElements(verticesArray);
+        int naoNull = 0;
+        for(int i = 0; i < this.ordem(); i++){
+            if(verticesArray[i] != null){
+                naoNull++;
+            }
+        }
+        if(naoNull <= ordem() && naoNull > 0){
+            return new GrafoCompleto("Subgrafo de " + this.nome, naoNull);
+        } else return null;
+    }
 }
