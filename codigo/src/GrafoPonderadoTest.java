8c03c5c1d8c2a73ae27437a9f2577c16ae61bbed
JVTemponi <jvictortemponi@gmail.com>
2022-10-01 21:10:28 -0300

    Testes classe GrafoPonderado

--- codigo/src/GrafoPonderadoTest.java
+++ codigo/src/GrafoPonderadoTest.java
@@ -1,9 +1,53 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
 public class GrafoPonderadoTest {
     
     @Test
-    public void TestCriarGrafoPonderado() {
-        
+    public void TestCriarGrafoPonderadoVerificandoOrdem() {
+
+        GrafoNaoPonderado grafoNaoPonderado = new GrafoNaoPonderado("Não Ponderado");
+        grafoNaoPonderado.addVertice(1);
+        grafoNaoPonderado.addVertice(2);
+        grafoNaoPonderado.addVertice(3);
+        grafoNaoPonderado.ordem();
+
+        assertEquals(3,grafoNaoPonderado.ordem());
+    }
+
+    @Test
+    public void TestVerificarGrafoPonderadoCompletoComOrdemPar() {
+
+        GrafoNaoPonderado grafoNaoPonderado = new GrafoNaoPonderado("Não Ponderado");
+        grafoNaoPonderado.addVertice(1);
+        grafoNaoPonderado.addVertice(2);
+        grafoNaoPonderado.addVertice(3);
+        grafoNaoPonderado.addVertice(4);
+
+        grafoNaoPonderado.addAresta(1, 2);
+        grafoNaoPonderado.addAresta(1, 3);
+        grafoNaoPonderado.addAresta(1, 4);
+        grafoNaoPonderado.addAresta(2, 3);
+        grafoNaoPonderado.addAresta(2, 4);
+        grafoNaoPonderado.addAresta(3, 4);
+
+        assertTrue(grafoNaoPonderado.completo());
+    }
+
+    @Test
+    public void TestVerificarGrafoPonderadoCompletoComOrdemImpar() {
+
+        GrafoNaoPonderado grafoNaoPonderado = new GrafoNaoPonderado("Não Ponderado");
+        grafoNaoPonderado.addVertice(1);
+        grafoNaoPonderado.addVertice(2);
+        grafoNaoPonderado.addVertice(3);
+
+        grafoNaoPonderado.addAresta(1, 2);
+        grafoNaoPonderado.addAresta(1, 3);
+        grafoNaoPonderado.addAresta(2, 3);
+
+        assertTrue(grafoNaoPonderado.completo());
     }
 }
