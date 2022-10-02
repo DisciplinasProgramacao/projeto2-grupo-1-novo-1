06466c082c06b4343551758f60ff83aace18fefe
MF300175 <111130192+MF300175@users.noreply.github.com>
2022-10-01 21:20:01 -0300

    sobrescrevendo tostring()

--- codigo/src/Vertice.java
+++ codigo/src/Vertice.java
@@ -1,4 +1,4 @@
-/** 
+/**
  * MIT License
  *
  * Copyright(c) 2021 João Caram <caram@pucminas.br>
@@ -10,7 +10,8 @@
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in all
+ * The above copyright notice and this permission notice shall be included in
+ * all
  * copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
@@ -23,73 +24,83 @@
  */
 
 public class Vertice {
-    
-    private ABB<Aresta> arestas;    
-    private final int  id;
+
+    private ABB<Aresta> arestas;
+    private final int id;
     private boolean visitado;
 
-/**
+    /**
      * Construtor para criação de vértice identificado
+     * 
      * @param id Número/id do vértice a ser criado (atributo final).
      */
-    public Vertice(int id){
+    public Vertice(int id) {
         this.id = id;
         this.arestas = new ABB<Aresta>();
         this.visitado = false;
     }
-    
-/**
-     * Adiciona uma aresta neste vértice para um destino
-     * @param peso Peso da aresta (0 para grafos não ponderados)
-     * @param destino Vértice de destino
-     * @return TRUE quando a aresta for adicionada
-     */
-    public boolean addAresta(int destino){
-        return this.arestas.add(destino,new Aresta(0, destino));
-    }
-
 
     /**
      * Adiciona uma aresta neste vértice para um destino
+     * 
+     * @param peso    Peso da aresta (0 para grafos não ponderados)
      * @param destino Vértice de destino
-     * @param peso Peso da aresta
      * @return TRUE quando a aresta for adicionada
      */
-    public boolean addAresta(int destino, int peso){
-        return this.arestas.add(destino,new Aresta(peso, destino));
+    public boolean addAresta(int destino) {
+        return this.arestas.add(destino, new Aresta(0, destino));
+    }
+
+    /**
+     * Adiciona uma aresta neste vértice para um destino
+     * 
+     * @param destino Vértice de destino
+     * @param peso    Peso da aresta
+     * @return TRUE quando a aresta for adicionada
+     */
+    public boolean addAresta(int destino, int peso) {
+        return this.arestas.add(destino, new Aresta(peso, destino));
     }
 
     /**
      * Verifica se já existe aresta entre este vértice e um destino. Método privado
+     * 
      * @param destino Vértice de destino
      * @return TRUE se existe aresta, FALSE se não
      */
-    public Aresta existeAresta(int destino){
+    public Aresta existeAresta(int destino) {
         return this.arestas.find(destino);
 
     }
-    
+
     /**
      * Retorna o grau do vértice
+     * 
      * @return Grau (inteiro não negativo)
      */
-    public int grau(){
+    public int grau() {
         return this.arestas.size();
     }
 
-    public void visitar(){
+    public void visitar() {
         this.visitado = true;
     }
-    
-    public void limparVisita(){
+
+    public void limparVisita() {
         this.visitado = false;
     }
-    
-    public boolean visitado(){
+
+    public boolean visitado() {
         return this.visitado;
     }
 
     public int getId() {
         return this.id;
     }
+
+    @Override
+    public String toString() {
+        return String.valueOf(this.id);
+    }
+
 }
