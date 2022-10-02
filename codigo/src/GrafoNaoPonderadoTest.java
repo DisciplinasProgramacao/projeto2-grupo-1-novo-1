8809d1e7442c46bc098a27b07340f45bc67aaad8
JVTemponi <jvictortemponi@gmail.com>
2022-10-01 21:15:45 -0300

    Ajustes Teste

--- codigo/src/GrafoNaoPonderadoTest.java
+++ codigo/src/GrafoNaoPonderadoTest.java
@@ -1,16 +1,53 @@
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class GrafoNaoPonderadoTest {
 
     @Test
-    public void TesteCriarGrafoMutavel() {
+    public void TestCriarGrafoPonderadoVerificandoOrdem() {
 
-        GrafoNaoPonderado grafoNPonderado = new GrafoNaoPonderado("Grafo N ponderado teste");
-        grafoNPonderado.addVertice(10);
-        grafoNPonderado.addVertice(15);
-        grafoNPonderado.addAresta(10, 15);
-        assertEquals(2, grafoNPonderado.ordem());
+        GrafoNaoPonderado grafoNaoPonderado = new GrafoNaoPonderado("Não Ponderado");
+        grafoNaoPonderado.addVertice(1);
+        grafoNaoPonderado.addVertice(2);
+        grafoNaoPonderado.addVertice(3);
+        grafoNaoPonderado.ordem();
+
+        assertEquals(3,grafoNaoPonderado.ordem());
+    }
+
+    @Test
+    public void TestVerificarGrafoPonderadoCompletoComOrdemPar() {
+
+        GrafoNaoPonderado grafoNaoPonderado = new GrafoNaoPonderado("Não Ponderado");
+        grafoNaoPonderado.addVertice(1);
+        grafoNaoPonderado.addVertice(2);
+        grafoNaoPonderado.addVertice(3);
+        grafoNaoPonderado.addVertice(4);
+
+        grafoNaoPonderado.addAresta(1, 2);
+        grafoNaoPonderado.addAresta(1, 3);
+        grafoNaoPonderado.addAresta(1, 4);
+        grafoNaoPonderado.addAresta(2, 3);
+        grafoNaoPonderado.addAresta(2, 4);
+        grafoNaoPonderado.addAresta(3, 4);
+
+        assertTrue(grafoNaoPonderado.completo());
+    }
+
+    @Test
+    public void TestVerificarGrafoPonderadoCompletoComOrdemImpar() {
+
+        GrafoNaoPonderado grafoNaoPonderado = new GrafoNaoPonderado("Não Ponderado");
+        grafoNaoPonderado.addVertice(1);
+        grafoNaoPonderado.addVertice(2);
+        grafoNaoPonderado.addVertice(3);
+
+        grafoNaoPonderado.addAresta(1, 2);
+        grafoNaoPonderado.addAresta(1, 3);
+        grafoNaoPonderado.addAresta(2, 3);
+
+        assertTrue(grafoNaoPonderado.completo());
     }
     
 }
